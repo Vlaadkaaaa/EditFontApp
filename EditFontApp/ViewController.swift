fa623d625d333c0b48a590fb932344c2e1313c24
Vladka <vlad.lymar@list.ru>
2022-08-23 19:42:25 +0500

    Создан alert и action для кнопки добавления текста в label

--- EditFontApp/ViewController.swift
+++ EditFontApp/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func addButton(_ sender: Any) {
+        let alert = UIAlertController(title: "Добавление текста", message: "Введите текст", preferredStyle: .alert)
+        let add = UIAlertAction(title: "Добавить", style: .default, handler: {_ in
+            guard let textInTextField = alert.textFields?.first?.text else {return}
+            self.textLabel.text = textInTextField
+        })
+        alert.addTextField()
+        alert.addAction(add)
+        present(alert, animated: true)
+    }
+    
 }
 
